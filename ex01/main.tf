23247bcd512511f6ca9b27bc89f54413d72d34ff
swati07010 <105536133+swati07010@users.noreply.github.com>
2024-11-19 19:09:10 +0530

    Update main.tf

--- ex01/main.tf
+++ ex01/main.tf
@@ -2,10 +2,9 @@ provider "azurerm" {
   features {}
   subscription_id = "a6c46565-0203-40d4-9199-78dd615c778e"
 }
- 
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-S2"
+ # Resource Group
+resource "azurerm_resource_group" "rg1" {
+  name     = "rg1-S2"
   location = "Canada Central"
 }
  
@@ -13,14 +12,14 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-S2"
   address_space       = ["192.168.0.0/19"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
 }
  
 # Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-S2"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.rg1.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["192.168.0.0/24"]
 }
@@ -28,28 +27,40 @@ resource "azurerm_subnet" "subnet" {
 # Network Security Group
 resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-S2"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
 }
+ 
+# Public IP Address
+resource "azurerm_public_ip" "vm_public_ip" {
+  name                = "vm-public-ip"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  allocation_method   = "Static"  # Changed to Static as required by Standard SKU
+  sku                  = "Standard"  # Specifying the Standard SKU for public IP
+  domain_name_label   = "myvm-public-ip"  # Optional: This will create a DNS name for the public IP
+}
+ 
  
 # Network Interface for Linux VM
 resource "azurerm_network_interface" "nic_linux" {
   name                = "nic-linux-vm"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
  
   ip_configuration {
     name                          = "ipconfig-linux"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id  # Associate Public IP
   }
 }
  
 # Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "linux_vm" {
   name                            = "linux-vm"
-  resource_group_name             = azurerm_resource_group.rg.name
-  location                        = azurerm_resource_group.rg.location
+  resource_group_name             = azurerm_resource_group.rg1.name
+  location                        = azurerm_resource_group.rg1.location
   size                            = "Standard_DS1_v2"
   admin_username                  = "adminuser"
   admin_password                  = "Password@123" # Replace with secure credentials
@@ -76,4 +87,9 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
   }
  
   custom_data = base64encode(file("user-data.sh"))
+}
+ 
+# Output Public IP Address
+output "vm_public_ip" {
+  value = azurerm_public_ip.vm_public_ip.ip_address
 }
